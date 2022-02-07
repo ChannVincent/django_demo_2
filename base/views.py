51d88a956a67d7bc50d359da9731544055e95b9b
Vincent Chann <>
2022-02-07 01:53:43 +0100

    create room form working

--- base/views.py
+++ base/views.py
@@ -1,19 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
-
+from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all() # database query
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+    rooms = Room.objects.all()  # database query
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
 
 def room(request, roomId):
     selectedRoom = None
-    selectedRoom = Room.objects.get(id=roomId) # database query
-    context = {'room': selectedRoom}
-    return render(request, 'base/room.html', context)
+    selectedRoom = Room.objects.get(id=roomId)  # database query
+    context = {"room": selectedRoom}
+    return render(request, "base/room.html", context)
+
 
 def createRoom(request):
-    context = {}
-    return render(request, 'base/room_form.html', context)
+    form = RoomForm()
+
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
