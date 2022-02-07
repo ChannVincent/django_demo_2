3574bfed166a1ff06cb5e537b4db8745490e1b38
Vincent Chann <>
2022-02-07 01:56:40 +0100

    order room model with the newest first

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
 from pydoc import describe
 from django.db import models
-from django.contrib.auth.models import User # already built in django
+from django.contrib.auth.models import User  # already built in django
 
 
 class Topic(models.Model):
@@ -17,6 +17,9 @@ class Room(models.Model):
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-updated", "-created"]
 
     def __str__(self):
         return self.name
